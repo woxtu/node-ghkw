70cdfa948fffa65528f93c644d63340be4875733
woxtu <woxtup@gmail.com>
2018-02-09 01:17:25 +0900

    Render markdown table

--- bin/ghkw.js
+++ bin/ghkw.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const program = require('commander')
+const tablemark = require('tablemark')
 const run = require('../lib/')
 ;
 (async () => {
@@ -13,5 +14,24 @@ const run = require('../lib/')
   const token = process.env.GITHUB_TOKEN || program.token
   const keywords = program.args
 
-  console.log(await run(token, keywords))
+  try {
+    let results = await run(token, keywords)
+    results.sort((a, b) => a.total_count < b.total_count)
+    results = results.map((item, n) => ({ rank: n + 1, ...item }))
+
+    console.log(tablemark(results, {
+      columns: [
+        { name: 'Rank', align: 'right' },
+        { name: 'Keyword', align: 'left' },
+        { name: 'Count', align: 'right' },
+      ],
+      stringify (value) {
+        return (typeof value === 'number')
+          ? value.toString().replace( /(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+          : value
+      },
+    }))
+  } catch (e) {
+    console.error(e)
+  }
 })()
