ce3f9825e132d01dc1a9a3df226d0d485cedcda9
woxtu <woxtup@gmail.com>
2018-02-10 17:45:14 +0900

    Refactor

--- utils/object-extension.js
+++ utils/object-extension.js
@@ -1,6 +1,7 @@
 Object.defineProperty(Object.prototype, 'pick', {
   value (...paths) {
-    return paths.reduce((object, path) => ({ [path]: this[path], ...object }), {})
+    return paths
+      .reduce((object, path) => ({ [path]: this[path], ...object }), {})
   }
 })
 
@@ -8,6 +9,6 @@ Object.defineProperty(Object.prototype, 'pickBy', {
   value (predicate) {
     return Object.entries(this)
       .filter(([k, v]) => predicate(k, v))
-      .reduce((obj, [k, v]) => ({ [k]: v, ...obj }), {})
+      .reduce((object, [k, v]) => ({ [k]: v, ...object }), {})
   }
 })
